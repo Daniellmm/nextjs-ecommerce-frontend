1a6fbfee7382a3f94ca6d2e883d5f41d98cb0296
Daniellmm <idowudanielcsc190400@gmail.com>
2025-07-09 20:08:50 -1100

    added the  cart btn and the favourite(bookmark) btn to the product cart

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,8 +1,27 @@
+'use client'
 import Image from 'next/image'
+import { useState } from 'react'
+
 
 export default function ProductCard({ product }) {
+
+    const [isTapped, setIsTapped] = useState(false)
+
     return (
-        <div className="bg-[#F0EEED] rounded-lg shadow-md p-4 h-[420px] w-[300px] flex flex-col justify-center items-center">
+        <div className="bg-[#F0EEED] relative rounded-lg shadow-md p-4 h-[420px] w-[300px] flex flex-col justify-center items-center">
+            <button 
+            className='absolute top-6 left-4 cursor-pointer'
+            onClick={() => setIsTapped(!isTapped)}>
+                {isTapped ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                    <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                </svg>
+                    :
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                    </svg>
+
+                }
+            </button>
             {product.images && product.images[0] && (
                 <Image
                     src={product.images[0]}
@@ -33,14 +52,26 @@ export default function ProductCard({ product }) {
                         {product.rating} / 5
                     </span>
                 </div>
-                <div className='flex flex-wrap gap-2 '>
-                    <span className="text-black text-xl font-semibold">${product.price}</span>
-                    {product.discount && (
-                        <span className="text-gray-400 text-xl line-through font-semibold">${product.discount}</span>
-                    )}
-                    {product.percentage && (
-                        <span className="text-red-600 bg-red-200 rounded-full px-2 py-1 text-md">{product.percentage}%</span>
-                    )}
+                <div className='flex justify-between items-center'>
+                    <div>
+                        <div className='flex flex-wrap gap-2 '>
+                            <span className="text-black text-xl font-semibold">${product.price}</span>
+                            {product.discount && (
+                                <span className="text-gray-400 text-xl line-through font-semibold">${product.discount}</span>
+                            )}
+                            {product.percentage && (
+                                <span className="text-red-600 bg-red-200 rounded-full px-2 py-1 text-md">{product.percentage}%</span>
+                            )}
+                        </div>
+                    </div>
+                    <div>
+                        <button className=' bg-black text-white p-2 rounded-full hover:bg-white hover:text-black cursor-pointer'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                            </svg>
+
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
